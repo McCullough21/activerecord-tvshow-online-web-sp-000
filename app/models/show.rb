e040f198e8c7551dfa0dab518d5c901979efb098
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-11 14:59:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,5 +9,14 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+  def self.least_popular_show
+    self.order(rating: :asc).first
+  end
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+  def self.popular_shows
+    self.all.where(":rating > ?", 5)
+  end
   
 end
